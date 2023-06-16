6b8ed0e51f5473cadccc6619b08b2e1061495dc3
jhanpedroza <136771311+jhanpedroza@users.noreply.github.com>
2023-06-15 21:09:11 -0500

    Add files via upload

---
+++ 2557860/store/index.php
@@ -0,0 +1,5 @@
+<?php
+
+require_once "ddbb/DBConexion.php";
+//require_once "controllers/productsCtrl.php";
+require_once "services/productsServices.php";
---
+++ 2557860/store/services/productsServices.php
@@ -0,0 +1,8 @@
+<?php
+
+// Call model
+require_once "objects/productsObjects.php";
+$products = ProductObjects::getAllProducts();
+
+$json_response = json_encode($products);
+echo $json_response;
